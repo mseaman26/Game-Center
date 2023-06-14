5afd832db0a724f9529d868e74522c2c3855e9c8
michael seaman <mseaman26@gmail.com>
2023-06-14 00:09:38 -0700

    hopefully have session storage set up with mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const sessionStore = new MongoStore({
+  mongooseConnection: mongoose.connection,
+  collection: 'sessions',
+});
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gameCenterDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection;
+module.exports = {
+  mongooseConnection: mongoose.connection,
+  sessionStore,
+};
--- server/server.js
+++ server/server.js
@@ -1,20 +1,40 @@
-const express = require('express')
-const routes = require('./routes')
-const db = require('./config/connection')
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3001
-const app = express()
+const routes = require('./routes');
+const { sessionStore } = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// Configure and use session middleware
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
-app.use(routes)
+app.use(routes);
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`now listening on port ${PORT}`)
-    })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gameCenterDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Now listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log('MongoDB connection error:', error);
+  });
