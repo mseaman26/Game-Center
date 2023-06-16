4aee6678f0344d87e08c7d5ce3ac95dcc8e73c3b
michael seaman <mseaman26@gmail.com>
2023-06-16 12:58:52 -0700

    moved login logout to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,21 @@
 import React, {useEffect, useState} from "react";
+import { Link } from 'react-router-dom'
 import Auth from '../utils/auth'
 
 const Header = () => {
+    const handleLogOut = () => {
+        Auth.logout()
+    }
     
-console.log(Auth.getToken())
+
 
     return (
         <>
             <h1>Header</h1>
             <p>logged in as: {Auth.getProfile()? Auth.getProfile().data.username : 'not logged in'}</p>
+            <Link to={'/login'}>Log In</Link>
+    <Link to={'/signup'}>Sign Up</Link>
+    <button onClick={handleLogOut}>Log Out</button>
         </>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+
 import Auth from '../utils/auth'
 
 
 
 const Home = () => {
 
-const handleLogOut = () => {
-    Auth.logout()
-}
+
 
 return (
     <>
     <h1>Home Page</h1>
-    <Link to={'/login'}>Log In</Link>
-    <Link to={'/signup'}>Sign Up</Link>
-    <button onClick={handleLogOut}>Log Out</button>
+    
     </>
 )
 }
