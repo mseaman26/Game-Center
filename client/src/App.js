9938cbb2cc14eed06c6d65f2a9890180af0a8c98
michael seaman <mseaman26@gmail.com>
2023-06-17 12:15:16 -0700

    mongodb uri

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home'
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config()
 
 
@@ -13,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 
 
