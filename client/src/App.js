44eef5d16f43787a6b7d8f04e781d8b83fc3b63d
michael seaman <mseaman26@gmail.com>
2023-06-17 12:41:53 -0700

    added header back

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp'
 function App() {
   return (
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
