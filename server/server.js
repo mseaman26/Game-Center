99fdbbba67dc0802e9d03be9b0cd519a665c289a
michael seaman <mseaman26@gmail.com>
2023-06-16 19:00:53 -0700

    redeploying

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config()
-const path = require('path');
+
 
 const routes = require('./routes');
 
@@ -14,9 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
-}
+
 
 app.use(routes);
 
