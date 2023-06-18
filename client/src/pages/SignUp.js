b7badd7adbc962fc7f5974261a85ccf5822f82f9
michael seaman <mseaman26@gmail.com>
2023-06-17 18:06:15 -0700

    another attempt

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -27,7 +27,7 @@ const SignupForm = () => {
         throw new Error(response.error);
       }
       // Handle successful signup, e.g., redirect to login page
-      Auth.login(response.token)
+      Auth.login(response)
     } catch (err) {
       console.error(err);
       setUserSignupError(true);
